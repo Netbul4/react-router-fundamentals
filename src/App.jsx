b10825ee40ddf514d775be1b7800b019f9223fe8
Netbul4 <eddiepp06@gmail.com>
2024-03-25 12:03:40 -0400

    feat: Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Header } from "./components/Header";
+import { Menu } from "./components/Menu";
 
 function App() {
   return (
     <>
       <Header />
       <BrowserRouter>
-        <nav className="menu">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact </Link>
-        </nav>
+        <Menu />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
