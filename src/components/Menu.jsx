cdb2d1a22087182cc72e66690d5e92f29e4dba39
Netbul4 <eddiepp06@gmail.com>
2024-03-25 12:13:01 -0400

    feat: Using NavLinks

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export function Menu() {
   return (
-    <nav className="menu">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact </Link>
-    </nav>
+    <>
+      <nav className="menu">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact </Link>
+      </nav>
+      <nav className="menu">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/contact"
+        >
+          Contact{" "}
+        </NavLink>
+      </nav>
+    </>
   );
 }
